c69b8652cfa7134793795da526024c688b5c110b
dziobak <31377653+sergserg2@users.noreply.github.com>
2017-11-05 18:06:31 +0100

    Update tvdb_scraper.py
    Separate exceptions for connection error and lack of IMDb ID in TVDb
    Merge tvshow/episode querries to one querry
    Other small modifications

--- resources/core/tvdb_scraper.py
+++ resources/core/tvdb_scraper.py
@@ -8,38 +8,39 @@
 #############################
 
 from common import *
+from requests.exceptions import ConnectionError
 import resources.support.tvdbsimple as tvdb
 tvdb.KEYS.API_KEY = "53F49B260156B636"
 
 def get_IMDb_ID(updateitem, tvdb_id):
-	imdb_id = None
-	statusInfo = "OK"
 	defaultLog( addonLanguage(32509) )
 	if tvdb_id == "" or tvdb_id == None:
-		return (imdb_id, "missing TVDB ID");
+		return (None, "missing TVDB ID");
 	if updateitem == "tvshow":
-		try:
 			show = tvdb.Series(tvdb_id)
-			response = show.info()
-			imdb_id = show.imdbId
-		except:
-			defaultLog( addonLanguage(32511) )
-			pass
 	elif updateitem == "episode":
-		try:
-			episode = tvdb.Episode(tvdb_id)
-			response = episode.info()
-			imdb_id = episode.imdbId
-		except:
-			defaultLog( addonLanguage(32511) )
-			pass
-		#if empty, return None
-		if imdb_id == "" or imdb_id == None:
-			imdb_id = None
-			statusInfo = "get_IMDb_ID: TVDB " + str( tvdb_id ) + " (" + updateitem + ") -> missing IMDb ID"
-		#special cases
-		if imdb_id != None:
-			if "tt" not in imdb_id: imdb_id = "tt" + str(imdb_id)
-			imdb_id = imdb_id.rstrip('/')
+			show = tvdb.Episode(tvdb_id)
+	else:
+		defaultLog( addonLanguage(32511) )
+		return (None, "bad item type :" + updateitem)
+	#connection
+	try:
+		response = show.info()
+	except ConnectionError as err:
+		defaultLog( "TVDB connection error: " + err )
+		return (imdb_id, "get_IMDb_ID: connection error: " + err)
+	#result
+	try:
+		imdb_id = show.imdbId
+	except:
+		imdb_id = None
+		pass
+	#no IMDb ID
+	if imdb_id == None or imdb_id == "":
+		defaultLog( addonLanguage(32511) )
+		return (None, "get_IMDb_ID: TVDB " + str( tvdb_id ) + " (" + updateitem + ") -> missing IMDb ID")
+	#special cases
+	if "tt" not in imdb_id: imdb_id = "tt" + str(imdb_id)
+	imdb_id = imdb_id.rstrip('/')
 	defaultLog( addonLanguage(32512) % ( imdb_id ) )
-	return (imdb_id, statusInfo)
+	return (imdb_id, "OK")
